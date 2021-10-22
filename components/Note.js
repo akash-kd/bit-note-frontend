86c58ddca0e65b15eb33a809bb3e5b9a8d19c380
Akash Kandasamy <akashram953@gmail.com>
2021-10-22 19:25:06 +0530

    chore

--- components/Note.js
+++ components/Note.js
@@ -4,24 +4,64 @@ import styles from '../styles/components/note.module.css'
 import * as bp3 from '@blueprintjs/core'
 import Editor from '@monaco-editor/react'
 import axios from 'axios'
+import { Editor as RichEditor, EditorState, RichUtils} from 'draft-js'
+import 'draft-js/dist/Draft.css'
 class Note extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      title: '',
+      desc: '',
+      content: '',
+      editorState: EditorState.createEmpty()
+    }
+    this.onChange = editorState => this.setState({editorState})
+    this.handleKeyCommand = this.handleKeyCommand.bind(this)
+    this._onBoldClick = this._onBoldClick.bind(this)
   }
 
   componentDidMount() {
     console.log('Note mounted')
-    axios.post('http://localhost:3030/note/')
+    axios.post('http://localhost:3030/note/getNote',{id:this.props.id})
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          title: res.data.title,
+          desc: res.data.desc,
+          code: res.data.code
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
   componentDidUpdate() {
     console.log('Note updated')
   }
 
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command)
+
+    if (newState) {
+      this.onChange(newState)
+      return 'handled'
+    }
+
+    return 'not-handled'
+  }
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
+  }
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'))
+  }
+
+
   render(){
     return(
       <bp3.Card className={styles.note}>
         <div className={styles.title + ' space-btw'}>
-          <h3>{this.props.title || 'How to create basic note'}</h3>
+          <h3>{this.props.title || this.state.title || 'How to create basic note'}</h3>
           <bp3.Popover 
             placement="bottom-right"
             minimal
@@ -46,13 +86,21 @@ class Note extends React.Component {
         </div>
         <bp3.Divider className={styles.divider}/>
         
-        <div className={styles.desc}>{this.props.desc}</div>
+        <div className={styles.desc}>
+          <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+          <button onClick={this._onItalic.bind(this)}>Italic</button>
+          <RichEditor 
+            editorState={this.state.editorState} 
+            onChange={this.onChange}
+            handleKeyCommand={this.handleKeyCommand} 
+          />
+        </div>
         <div className={styles.code}>    
           <Editor
             width="auto"
             theme="vs-dark"
             defaultLanguage="javascript"
-            defaultValue={this.props.code || '// let\'s write some broken code 😈 '}
+            defaultValue={this.props.code ||  this.state.code || '// let\'s write some broken code 😈 '}
             onChange={(newValue, e) => console.log(newValue, e)}
           />
         </div>
